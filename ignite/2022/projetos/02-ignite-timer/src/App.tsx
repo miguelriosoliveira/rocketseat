4b61ad35e5334d77555b9584d52b28fc09f3fdb6
Miguel Rios <miguelriosoliveira@gmail.com>
2022-09-27 22:19:25 +0200

    chore: Global Styles setup

--- ignite/2022/projetos/02-ignite-timer/src/App.tsx
+++ ignite/2022/projetos/02-ignite-timer/src/App.tsx
@@ -1,6 +1,12 @@
 import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
+import { GlobalStyles } from './styles';
+import { defaultTheme } from './styles/themes';
 
 export function App() {
-  return <ThemeProvider theme={defaultTheme}>hello world</ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      hello world
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
