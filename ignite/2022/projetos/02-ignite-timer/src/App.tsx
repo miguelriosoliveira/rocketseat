4dec447cab8a62958471da3728c8684b68e44fe9
Miguel Rios <miguelriosoliveira@gmail.com>
2022-09-28 01:11:25 +0200

    feature: Routes layout

--- ignite/2022/projetos/02-ignite-timer/src/App.tsx
+++ ignite/2022/projetos/02-ignite-timer/src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 
+import { Router } from './Router';
 import { GlobalStyles } from './styles';
 import { defaultTheme } from './styles/themes';
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			hello world
+			<Router />
 			<GlobalStyles />
 		</ThemeProvider>
 	);
