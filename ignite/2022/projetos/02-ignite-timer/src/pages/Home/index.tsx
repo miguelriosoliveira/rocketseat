22bffdc2ec2a8e632adb8eadec643800b7dd94ad
Miguel Rios <miguelriosoliveira@gmail.com>
2022-09-28 12:29:47 +0200

    feature: Styling Home (neat details to inputs)

--- ignite/2022/projetos/02-ignite-timer/src/pages/Home/index.tsx
+++ ignite/2022/projetos/02-ignite-timer/src/pages/Home/index.tsx
@@ -15,10 +15,24 @@ export function Home() {
 		<HomeContainer>
 			<FormContainer id="task-form">
 				<label htmlFor="task">Vou trabalhar em</label>
-				<TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+				<TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+
+				<datalist id="task-suggestions">
+					<option value="projeto 1" />
+					<option value="projeto 2" />
+					<option value="projeto 3" />
+					<option value="banana" />
+				</datalist>
 
 				<label htmlFor="minutes-amount">durante</label>
-				<MinutesAmountInput type="number" id="minutes-amount" placeholder="00" />
+				<MinutesAmountInput
+					type="number"
+					id="minutes-amount"
+					placeholder="00"
+					step={5}
+					min={5}
+					max={60}
+				/>
 
 				<span>minutos.</span>
 			</FormContainer>
