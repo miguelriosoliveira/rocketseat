d6283fbd3c4e87c3b444462c9262ead782e127a6
Miguel Rios <miguelriosoliveira@gmail.com>
2022-09-28 02:11:19 +0200

    feature: Styling Home (form, countdown)

--- ignite/2022/projetos/02-ignite-timer/src/pages/Home/index.tsx
+++ ignite/2022/projetos/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,3 +1,32 @@
+import { Play } from 'phosphor-react';
+
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from './styles';
+
 export function Home() {
-	return <h1>home</h1>;
+	return (
+		<HomeContainer>
+			<FormContainer id="task-form">
+				<label htmlFor="task">Vou trabalhar em</label>
+				<input id="task" />
+
+				<label htmlFor="minutes-amount">durante</label>
+				<input type="number" id="minutes-amount" />
+
+				<span>minutos.</span>
+			</FormContainer>
+
+			<CountdownContainer>
+				<span>0</span>
+				<span>0</span>
+				<Separator>:</Separator>
+				<span>0</span>
+				<span>0</span>
+			</CountdownContainer>
+
+			<button type="submit" form="task-form">
+				<Play size={24} />
+				Começar
+			</button>
+		</HomeContainer>
+	);
 }
